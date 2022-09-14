dd12d1c324349adaf658b74a9e354b2a7362adac
LacoLi <gomsemarie@gmail.com>
2022-09-14 17:12:52 +0900

    zzz

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import BlackyEngine from "./library/BlackyEngine";
+import { Util } from "./library/BlackyEngine/common/util";
 
 let gameManager: BlackyEngine;
 
 window.onload = () => {
   gameManager = new BlackyEngine("game", 2);
   gameManager.start();
+
+  let arr = [{ num: 3 }, { num: 1 }, { num: 7 }, { num: 3 }, { num: 4 }];
+  let res = Util.Sort.quick(arr, (element) => element.num);
+  console.log(res);
 };
--- src/library/BlackyEngine/common/util/index.ts
+++ src/library/BlackyEngine/common/util/index.ts
@@ -21,6 +21,31 @@ namespace Util {
       return new Vector2(a.getX() * b.getX(), a.getY() * b.getY());
     }
   }
+
+  export namespace Sort {
+    export function quick<T>(arr: T[], getter: (element: T) => any): T[] {
+      if (arr.length < 2) {
+        return arr;
+      }
+
+      const pivot = [arr[0]];
+      const left = [];
+      const right = [];
+
+      for (let i = 1; i < arr.length; i++) {
+        if (getter(arr[i]) < getter(pivot[0])) {
+          left.push(arr[i]);
+        } else if (getter(arr[i]) > getter(pivot[0])) {
+          right.push(arr[i]);
+        } else {
+          pivot.push(arr[i]);
+        }
+      }
+
+      console.log(`left: ${left}, pivot: ${pivot}, right: ${right}`);
+      return quick(left, getter).concat(pivot, quick(right, getter));
+    }
+  }
 }
 
 export { Util };
